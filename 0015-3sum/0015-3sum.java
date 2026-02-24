c8aa9ee1fec058ca6be371e6788af85754c7374c
RISHIKESH KUMAR <54925850+thakurrisu@users.noreply.github.com>
2026-02-24 23:37:40 +0530

    Time: 33 ms (71.79%), Space: 59.2 MB (75.24%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,22 +1,26 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        HashSet<List<Integer>> ans = new HashSet<>();
+        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
         for(int i=0;i<=nums.length-2;i++){
+            if(i!=0 && nums[i]== nums[i-1]) continue;
             int j =i+1 , k = nums.length-1;
             int target = nums[i]*-1;
             while(j<k){
                  int sum = nums[j]+nums[k];
-                 if(sum == target && !ans.contains(new ArrayList<>(Arrays.asList(nums[i],nums[j],nums[k]))))
-                    { ans.add(new ArrayList<>(Arrays.asList(nums[i],nums[j],nums[k])));
-                    System.out.println(i +"" + j +""+k);
+                 if(sum == target)
+                    { ans.add(new ArrayList<>(Arrays.asList(nums[i],nums[j],nums[k])));                   
+                     while(j<k && nums[j]==nums[j+1] ) j++;
+                      while(j<k && nums[k]==nums[k-1] ) k--;
+                       j++;
+                           k--;
                     }
-                   if(target>=sum)
+                  else if(target>=sum)
                       j++;
                     else 
                      k--; 
             }
         }
-        return ans.stream().collect(Collectors.toList());
+        return ans;
     }
 }
