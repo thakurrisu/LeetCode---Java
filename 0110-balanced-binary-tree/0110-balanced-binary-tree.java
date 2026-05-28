8f746bb2e9b2bebffc57f60c5b2ea60a6d0d42b5
RISHIKESH KUMAR <54925850+thakurrisu@users.noreply.github.com>
2026-05-28 22:48:14 +0530

    Time: 1 ms (28.9%), Space: 45.6 MB (57.91%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -14,18 +14,22 @@
  * }
  */
 class Solution {
-    public static int isBalancedHelper(TreeNode root) {
+    public static int isBalancedHelper(TreeNode root , int[] max) {
         if(root == null) return 0;
-        int lh = isBalancedHelper(root.left);
-        int rh = isBalancedHelper(root.right);
-        int  h = Math.max(lh,rh) + 1;
-       return h;
+        int lh = isBalancedHelper(root.left , max);
+        int rh = isBalancedHelper(root.right, max);
+
+        max[0] = Math.max(max[0] , Math.abs(lh-rh));
+       return Math.max(lh,rh) + 1;
     }
     public boolean isBalanced(TreeNode root) {
         if(root == null) return true;
-        int lh = isBalancedHelper(root.left);
-        int rh = isBalancedHelper(root.right);
-        if(Math.abs(lh-rh)>1) return false;
-        return isBalanced(root.left) && isBalanced(root.right);
+        int[] max = new int[1];
+        int lh = isBalancedHelper(root ,max);
+        if(max[0]>1) return false;
+        return true;
+        // int rh = isBalancedHelper(root.right);
+        // if(Math.abs(lh-rh)>1) return false;
+        // return isBalanced(root.left) && isBalanced(root.right);
     }
 }
